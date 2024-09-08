950c2355b8e953ed98de01559a65979cf2f3971c
Muhammad Aryan <160228293+MuhammadAriyan@users.noreply.github.com>
2024-09-08 19:27:01 +0500

    Update calculator.tsx

--- components/calculator.tsx
+++ components/calculator.tsx
@@ -37,19 +37,24 @@ const Calculator = () => {
             <Button>=</Button>
           </div>
         </CardContent>
-        <CardFooter className="flex justify-between items-center p-4">
-          <div className="text-sm font-medium flex items-center space-x-2 p-1 sm:p-0">
-            MADE WITH <Heart className="text-red-500 hover:fill-red-500 m-1 " size={16} /> BY MUHAMMAD ARYAN <Swords size={16} className='hover:text-orange-600 ease-in-out' />
-          </div>
-          <div className="flex space-x-2">
-            <Link href={'https://github.com/MuhammadAriyan'}>
-              <img src="github.png" alt="GITHUB" width={30} className="opacity-75 hover:opacity-100 transition duration-200 ease-in-out" />
-            </Link>
-            <Link href={'https://www.linkedin.com/in/muhammad-aryan'}>
-              <LinkedinIcon className=" hover:opacity-100 transition duration-200 ease-in-out" size={30} />
-            </Link>
-          </div>
-        </CardFooter>
+        <CardContent />
+<CardFooter className="flex flex-col sm:flex-row justify-between items-center p-4">
+  <div className="text-sm font-medium flex flex-col sm:flex-row items-center space-x-2 p-1 sm:p-0 text-center sm:text-left">
+    <span>MADE WITH</span>
+    <Heart className="text-red-500 hover:fill-red-500 m-1" size={16} />
+    <span>BY MUHAMMAD ARYAN</span>
+    <Swords size={16} className="hover:text-orange-600 ease-in-out" />
+  </div>
+  <div className="flex mt-2 sm:mt-0 space-x-2">
+    <Link href="https://github.com/MuhammadAriyan">
+      <img src="github.png" alt="GITHUB" width={30} className="opacity-75 hover:opacity-100 transition duration-200 ease-in-out" />
+    </Link>
+    <Link href="https://www.linkedin.com/in/muhammad-aryan">
+      <LinkedinIcon className="hover:opacity-100 transition duration-200 ease-in-out" size={30} />
+    </Link>
+  </div>
+</CardFooter>
+
       </Card>
     </div>
   )
